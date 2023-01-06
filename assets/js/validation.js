a5e9d5b07cd4d93ee5a28b945b65beeeb87c6f5e
akashpmani <akashpmani@gmail.com>
2023-01-06 12:17:20 +0530

    nameeditor

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,13 +1,13 @@
 
 function submitbtn(){
     let btn
-    var Name=document.getElementById("name").value.trim()
+    var name=document.getElementById("name").value.trim()
     var mail=document.getElementById("email").value.trim()
     var mob=document.getElementById("mobile").value.trim()
     var message=document.getElementById("message").value.trim()
     var sub=document.getElementById("subject").value.trim()
 
-    if(Name===""||mail===""||mob===""||message==="" || sub==="")
+    if(name===""||mail===""||mob===""||message==="" || sub==="")
     {
         alert("Please fill all fields")
         btn =false;
@@ -22,6 +22,12 @@ function submitbtn(){
         alert("Please enter your phone number correctly")
         btn =false;
      }
+     else if(!/^[a-zA-Z]$/.test(name))
+     {
+        alert("Please enter your phone number correctly")
+        btn =false;
+     }
+
 
     return btn;
 
